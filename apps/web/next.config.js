6115d02c9d06f93d23be0422f87549abbdbdd64b
Jokyuseong <pandora_h@naver.com>
2023-05-21 19:03:46 +0900

    Feat/vanilla extract (#16)
    * pnpm major version update 7 -> 8
    
    * add vanilla extract
    
    * renew pnpm-lock.yaml
    
    * add the appDir option to the next.config.js
    
    * add experiments key
    
    * add vanilla extract to apps/web too

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
-module.exports = {
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  experimental: {
+    appDir: true,
+  },
 };
+
+module.exports = withVanillaExtract(nextConfig);
