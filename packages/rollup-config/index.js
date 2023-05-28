8c987f5f6a00891de308b1b0ed584ca9bad60b48
LeeSeungHun <dos07008@naver.com>
2023-05-28 16:53:42 +0900

    rollup-config 패키지 개발

---
+++ packages/rollup-config/index.js
@@ -0,0 +1,29 @@
+const esbuild = require("rollup-plugin-esbuild");
+const preserveDirectives = require("rollup-plugin-preserve-directives");
+const typescript = require("@rollup/plugin-typescript");
+
+module.exports = {
+  input: "./src/index.tsx",
+  output: {
+    dir: "dist",
+    format: "esm",
+    preserveModules: true,
+  },
+  plugins: [
+    typescript.default(),
+    esbuild.default({
+      minify: process.env.NODE_ENV === "production",
+      jsxFactory: "React.createElement",
+      jsxFragment: "React.Fragment",
+      define: {
+        __VERSION__: '"x.y.z"',
+      },
+      tsconfig: "tsconfig.json",
+      loaders: {
+        ".json": "json",
+        ".js": "jsx",
+      },
+    }),
+    preserveDirectives.default(),
+  ],
+};
