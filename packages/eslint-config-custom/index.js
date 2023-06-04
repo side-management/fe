41a26ff0d054d0e4e154680dd1487c61dc00fa25
LeeSeungHun <dos07008@naver.com>
2023-06-04 21:23:28 +0900

    lint 설정

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,25 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "turbo", "airbnb", "plugin:prettier/recommended"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
+    "react/react-in-jsx-scope": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/prefer-default-export": "off",
+    "import/extensions": "off",
+    "react/button-has-type": "off",
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+    "react-hooks/exhaustive-deps": [
+      "error",
+      {
+        additionalHooks: "(useCustomHook)",
+      },
+    ],
   },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
+  ignorePatterns: ["**/dist/**/*"],
 };
