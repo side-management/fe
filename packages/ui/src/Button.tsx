eb57c2b2d92ab3fb2f42ac094f3571e9acc538c1
LeeSeungHun <dos07008@naver.com>
2023-06-08 21:02:50 +0900

    ui패키지 lint 설정

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -10,7 +10,7 @@ export const Button = ({
   leftSlot: ReactNode;
 }) => {
   return (
-    <button onClick={() => alert("boop")}>
+    <button onClick={() => {}}>
       {leftSlot}
       <span>{children}</span>
     </button>
