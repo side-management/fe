82747bc82f1d5851641ee673534c7edaa17483d3
catca <kdg96g@gmail.com>
2023-06-11 15:16:59 +0900

    test: hooks vitest setup (#13)

--- apps/docs/tests/example/example.spec.ts
+++ apps/docs/tests/example/example.spec.ts
@@ -1,8 +1,22 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
+import { act, renderHook } from "@testing-library/react";
 
-import { add } from "./example";
+import { add, useCounter } from "./example";
 
-it("배열 안의 숫자를 모두 더함", () => {
-  const result = add([1, 2, 3, 4, 5]);
-  expect(result).toBe(15);
+describe("add", () => {
+  it("배열 안의 숫자를 모두 더함", () => {
+    const result = add([1, 2, 3, 4, 5]);
+    expect(result).toBe(15);
+  });
+});
+
+describe("useCounter", () => {
+  it("should increment counter", () => {
+    const { result } = renderHook(() => useCounter());
+    console.log(result);
+    act(() => {
+      result.current.increment();
+    });
+    expect(result.current.count).toBe(1);
+  });
 });
--- apps/docs/tests/example/example.ts
+++ apps/docs/tests/example/example.ts
@@ -1,3 +1,11 @@
+import { useCallback, useState } from "react";
+
+export function useCounter() {
+  const [count, setCount] = useState(0);
+  const increment = useCallback(() => setCount((x) => x + 1), []);
+  return { count, increment };
+}
+
 export function add(numbers: number[]) {
   let sum = 0;
 
