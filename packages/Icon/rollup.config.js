464872251a60d0a7aca7110f2d3bbaacba4ef3df
LeeSeungHun <dos07008@naver.com>
2023-06-12 22:21:16 +0900

    불필요한 패키지 제거

--- packages/Icon/rollup.config.js
+++ packages/Icon/rollup.config.js
@@ -1,7 +1,3 @@
 import { getRollupConfig } from "rollup-config";
-import svgr from "@svgr/rollup";
 
-export default getRollupConfig((config) => ({
-  ...config,
-  plugins: [...config.plugins, svgr()],
-}));
+export default getRollupConfig();
